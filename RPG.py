b7e9e7932f0dded461d531925d67f34a42e1075c
Nabil Darwish <darwish070101@gmail.com>
2024-09-25 14:35:52 +0100

    Add files via upload

--- RPG.py
+++ RPG.py
@@ -7,19 +7,37 @@ import pygame
 import threading
 
 class Unit:
-    def __init__(self, name, hp, strength, defense, dexterity):
+    def __init__(self, name, player, physical, max_hp, strength, defense, resistance, dexterity, speed, luck, hp = None):
         self.name = name
-        self.hp = hp
+        self.player = player
+        self.physical = physical
+        self.max_hp = max_hp
+        self._hp = max_hp if hp is None else min(max_hp, hp)
         self.strength = strength
         self.defense = defense
+        self.resistance = resistance
         self.dexterity = dexterity
+        self.speed = speed
+        self.luck = luck
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, value):
+        self._hp = max(0, min(value, self.max_hp))
 
     def __str__(self):
         return f"""Name: {self.name}
-HP  : {self.hp}   
+PHYS: {self.physical}
+HP  : {self.hp}/{self.max_hp}   
 STR : {self.strength} 
 DEF : {self.defense}
-DEX : {self.dexterity} \n"""
+RES : {self.resistance}
+DEX : {self.dexterity}
+SPD : {self.speed} 
+LCK : {self.luck}\n"""
     
     def attack(self, enemy):
         input("Press Enter to continue \n")                 ##Might need to remove this later
@@ -27,14 +45,23 @@ DEX : {self.dexterity} \n"""
         hitChance = 70 + self.dexterity
         randomVariable = random.randint(0, 100)
         if randomVariable < hitChance:
-            damage = self.strength - enemy.defense
+            randomVariable = random.randint(0, 100)
+            if self.physical:
+                damage = self.strength - enemy.defense
+            else:
+                damage = self.strength - enemy.resistance
             damage = not_less_zero(damage)
+            if randomVariable < self.luck:                  ## Critical Hit
+                damage = damage * 2
             print(f"{self.name} hits {enemy.name} for {damage} ({hitChance})!")
             enemy.hp -= damage
             enemy.hp = not_less_zero(enemy.hp)
             print(enemy)
         else:
             print("Miss!")
+
+class Party:
+    def __init__
 
 class CombatManager:
     def __init__(self, player_unit, enemy_unit):
@@ -43,15 +70,18 @@ class CombatManager:
 
     def start_battle(self):
         while self.player_unit.hp > 0 and self.enemy_unit.hp > 0:
-            self.player_turn()
-            if self.enemy_unit.hp <= 0:
-                print("Enemy Defeated!")
-                return 0
-
-            self.enemy_turn()
-            if self.player_unit.hp <= 0:
-                print(f"{self.player_unit.name} has been defeated!")
-                return 1
+            turnOrder = sorted([self.player_unit, self.enemy_unit], key = lambda unit: unit.speed, reverse = True)
+            for unit in turnOrder:
+                if unit.player:
+                    self.player_turn()
+                    if self.enemy_unit.hp <= 0:
+                        print("Enemy Defeated!")
+                        return 0
+                else:
+                    self.enemy_turn()
+                    if self.player_unit.hp <= 0:
+                        print(f"{self.player_unit.name} has been defeated!")
+                        return 1
 
     def player_turn(self):
         self.player_unit.attack(self.enemy_unit)
@@ -62,39 +92,50 @@ class CombatManager:
 def not_less_zero(variable):
     return max(0, variable)
 
-def battle(playerUnit, enemyUnit):
-    while enemyUnit.hp > 0:
-        playerUnit.attack(enemyUnit)
+def pick_random_from(l):
+    randomVariable = random.randint(0, len(l))
+    return l[randomVariable]
+
+def start_music_thread(stop_event, sound_file):
+    music_thread = threading.Thread(target=looping_music, args=(stop_event, sound_file), daemon=True)
+    music_thread.start()
+
+def stop_current_music_thread(stop_event):
+    stop_event.set()
+    time.sleep(0.1)
 
 def looping_music(stop_event, sound_file):
-    pygame.mixer.init()
     pygame.mixer.music.load(sound_file)
     pygame.mixer.music.play(-1)  # Play music in a loop
     while not stop_event.is_set():
         time.sleep(0.1)
     pygame.mixer.music.stop()
 
+def stop_and_play_music(sound_file):
+    pygame.mixer.music.load(sound_file)
+    pygame.mixer.music.play()
+
 
 def main():
-    name = input("What's your name? \n")
+    pygame.mixer.init()
     stop_event = threading.Event()
-    music_thread = threading.Thread(target=looping_music, args=(stop_event, "music/riff.wav"), daemon=True)
-    music_thread.start()
-    playerUnit = Unit(name, 5, 6, 2, 10)
-    enemyUnit = Unit("Enemy", 5, 6, 3, 10)
+    start_music_thread(stop_event, "music/cats.wav")
+    name = input("What's your name? \n")
+    stop_current_music_thread(stop_event)
+    stop_event = threading.Event()
+    start_music_thread(stop_event, "music/riff.wav")
+    playerUnit = Unit(name = name, player = True, physical = False, max_hp = 5, strength = 6, defense = 2, resistance = 8, dexterity = 30, speed = 3, luck = 100)
+    enemyUnit = Unit("Enemy", False, False, 5, 6, 3, 0, 10, 5, 5)
     print(playerUnit)
     print(enemyUnit)
     combat = CombatManager(playerUnit, enemyUnit)
     outcome = combat.start_battle()
-    stop_event.set()
-    time.sleep(0.1)
+    stop_current_music_thread(stop_event)
     if outcome == 0:
-        pygame.mixer.music.load('music/victory.wav')
-        pygame.mixer.music.play()
+        stop_and_play_music('music/victory.wav')
         input("Congatulations\n")
     else:
-        pygame.mixer.music.load('music/game over.wav')
-        pygame.mixer.music.play()
+        stop_and_play_music('music/game over.wav')
         input("Sorry! Try again!\n")
     
 
